4f89b4707b851a0fb4d782d241cb14e28588ae93
rodaquino-OMNI <rodrigotaquino@gmail.com>
2025-04-25 07:37:11 -0300

    File: src/web/web/src/pages/404.tsx

---
+++ src/web/web/src/pages/404.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFoundPage: React.FC = () => {
+  return (
+    <div>
+      <h1>404 - Página Não Encontrada</h1>
+      <p>Desculpe, a página que você está procurando não existe.</p>
+    </div>
+  );
+};
+
+export default NotFoundPage;
