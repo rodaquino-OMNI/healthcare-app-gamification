89bf5c30276733f640b7f34ce5fb5361e65df352
rodaquino-OMNI <rodrigotaquino@gmail.com>
2025-04-25 07:25:50 -0300

    File: src/web/mobile/src/screens/home/index.ts

---
+++ src/web/mobile/src/screens/home/index.ts
@@ -0,0 +1,13 @@
+import HomeScreen from './Home';
+import Achievements from './Achievements';
+import Notifications from './Notifications';
+import ProfileScreen from './Profile';
+import SettingsScreen from './Settings';
+
+export {
+    HomeScreen,
+    Achievements,
+    Notifications,
+    ProfileScreen,
+    SettingsScreen,
+};
