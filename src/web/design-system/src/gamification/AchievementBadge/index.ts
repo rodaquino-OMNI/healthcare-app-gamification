46f56fe57af6eeb7954d878cf2810aadcecf17f0
rodaquino-OMNI <rodrigotaquino@gmail.com>
2025-04-25 07:09:55 -0300

    File: src/web/design-system/src/gamification/AchievementBadge/index.ts

---
+++ src/web/design-system/src/gamification/AchievementBadge/index.ts
@@ -0,0 +1,5 @@
+import { AchievementBadge } from './AchievementBadge';
+import type { AchievementBadgeProps } from './AchievementBadge';
+
+export type { AchievementBadgeProps };
+export default AchievementBadge;
