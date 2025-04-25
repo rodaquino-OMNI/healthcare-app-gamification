0f4d4e99c985f3392938825870027754aea97ed9
rodaquino-OMNI <rodrigotaquino@gmail.com>
2025-04-25 07:30:42 -0300

    File: src/web/shared/utils/index.ts

---
+++ src/web/shared/utils/index.ts
@@ -0,0 +1,19 @@
+/**
+ * Utility Functions Index
+ * 
+ * This barrel file re-exports all utility functions from the utils directory,
+ * providing a single point of import for all shared utilities used throughout
+ * the AUSTA SuperApp web applications.
+ */
+
+// Re-export date utility functions
+export * from './date';
+
+// Re-export formatting utility functions
+export * from './format';
+
+// Re-export validation schemas
+export * from './validation';
+
+// Re-export constants
+export * from '../constants';
