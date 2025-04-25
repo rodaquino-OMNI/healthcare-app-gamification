3a96d29c267606fa5e7116af29cd68981cb56a23
rodaquino-OMNI <rodrigotaquino@gmail.com>
2025-04-25 07:23:33 -0300

    File: src/web/mobile/src/navigation/index.tsx

---
+++ src/web/mobile/src/navigation/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'; // React v18.2.0
+import { AuthNavigator } from './AuthNavigator'; // src/web/mobile/src/navigation/AuthNavigator.tsx
+import { CareNavigator } from './CareNavigator'; // src/web/mobile/src/navigation/CareNavigator.tsx
+import { HealthNavigator } from './HealthNavigator'; // src/web/mobile/src/navigation/HealthNavigator.tsx
+import { MainNavigator } from './MainNavigator'; // src/web/mobile/src/navigation/MainNavigator.tsx
+import { PlanNavigator } from './PlanNavigator'; // src/web/mobile/src/navigation/PlanNavigator.tsx
+import { RootNavigator } from './RootNavigator'; // src/web/mobile/src/navigation/RootNavigator.tsx
+
+// IE3: Be generous about your exports so long as it doesn't create a security risk.
+export {
+  AuthNavigator, // Navigation component for authentication flow.
+  CareNavigator, // Navigation component for the Care Now journey.
+  HealthNavigator, // Navigation component for the My Health journey.
+  MainNavigator, // Navigation component for the main app flow (after authentication).
+  PlanNavigator, // Navigation component for the My Plan & Benefits journey.
+  RootNavigator, // Root navigation component that determines whether to show the auth flow or the main app.
+};
